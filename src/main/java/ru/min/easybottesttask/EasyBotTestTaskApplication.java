4a23f0172c0dfda030b6a6bcdfeefe8c96b54011
MarselMingaripov <bard1812@gmail.com>
2023-06-02 13:15:46 +0300

    init commit

---
+++ src/main/java/ru/min/easybottesttask/EasyBotTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.min.easybottesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBotTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyBotTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/min/easybottesttask/EasyBotTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.min.easybottesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBotTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
